e886bf693b9214261698f429c0234d486500cadf
Ziyue Pan <3180105354@zju.edu.cn>
2020-12-04 00:14:40 +0800

    update dev

--- IamClockApp/app/src/main/java/com/iamclock/iamclockapp/Fragments/Clock/AddClock.java
+++ IamClockApp/app/src/main/java/com/iamclock/iamclockapp/Fragments/Clock/AddClock.java
@@ -1,21 +1,128 @@
 package com.iamclock.iamclockapp.Fragments.Clock;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-import androidx.annotation.IntDef;
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.iamclock.iamclockapp.R;
+import com.iamclock.iamclockapp.Utils.CommonUtils;
 
-import java.lang.annotation.Retention;
+import java.util.Arrays;
 
 public class AddClock extends AppCompatActivity {
+
+    private TimePicker time_picker;
+    private CardView card_view_repeat;
+    private CardView card_view_label;
+    private Button button_cancel, button_confirm;
+
+    private final CharSequence[] repeat_items = {"周日", "周一", "周二", "周三", "周四", "周五", "周六"};
+    private boolean[] repeat_choice = {false, false, false, false, false, false, false};
+    private String label = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_clock);
+
+        InitTimePicker();
+        InitCardViewRepeat();
+        InitCancelConfirm();
+        InitLabel();
     }
 
+
+    private void InitTimePicker() {
+        // get and set time picker
+        time_picker = findViewById(R.id.time_picker);
+        time_picker.setIs24HourView(true);
+    }
+
+    private void InitCardViewRepeat() {
+        // get the repeat card view
+        card_view_repeat = findViewById(R.id.add_clock_repeat);
+        card_view_repeat.setClickable(true);
+        card_view_repeat.setOnClickListener(v -> {
+
+            new MaterialAlertDialogBuilder(this)
+                    .setTitle("重复")
+                    .setMultiChoiceItems(repeat_items, repeat_choice, (dialog, which, isChecked) -> {
+                        repeat_choice[which] = isChecked;
+                    })
+                    .setPositiveButton("确认", (dialogInterface, i) -> {
+                        TextView repeat_text = findViewById(R.id.repeat_string);
+
+                        if (CommonUtils.CheckBooleanArrayAll(repeat_choice, true)) {
+                            repeat_text.setText("每日");
+                        } else if (CommonUtils.CheckBooleanArrayAll(repeat_choice, false)) {
+                            repeat_text.setText("永不");
+                        } else if (repeat_choice[0]
+                                && !repeat_choice[1]
+                                && !repeat_choice[2]
+                                && !repeat_choice[3]
+                                && !repeat_choice[4]
+                                && !repeat_choice[5]
+                                && repeat_choice[6]) {
+                            repeat_text.setText("周末");
+                        } else if (!repeat_choice[0]
+                                && repeat_choice[1]
+                                && repeat_choice[2]
+                                && repeat_choice[3]
+                                && repeat_choice[4]
+                                && repeat_choice[5]
+                                && !repeat_choice[6]) {
+                            repeat_text.setText("工作日");
+                        } else {
+                            repeat_text.setText(CommonUtils.GenerateRepeatString(repeat_choice));
+                        }
+                    })
+                    .setNegativeButton("取消", (dialogInterface, i) -> {
+                        Arrays.fill(repeat_choice, false);
+                    }).show();
+        });
+    }
+
+    private void InitCancelConfirm() {
+        button_cancel = findViewById(R.id.cancel_button);
+        button_confirm = findViewById(R.id.confirm_button);
+
+        button_cancel.setOnClickListener(v -> finish());
+
+        button_confirm.setOnClickListener(v -> {
+            // TODO add confirm listener
+            System.out.println("confirm button clicked");
+        });
+    }
+
+    private void InitLabel() {
+        card_view_label = findViewById(R.id.add_clock_label);
+        card_view_label.setClickable(true);
+        card_view_label.setOnClickListener(v -> {
+            final EditText input = new EditText(this);
+            input.setInputType(InputType.TYPE_CLASS_TEXT);
+            input.setSingleLine(true);
+            input.setText(label.isEmpty() ? "无" : label);
+
+            new MaterialAlertDialogBuilder(this)
+                    .setTitle("设置标签")
+                    .setView(input)
+                    .setPositiveButton("确认", (dialogInterface, i) -> {
+                        label = input.getText().toString();
+                    }).show();
+        });
+
+    }
+
+
+    // TODO set all label clickable
 }
