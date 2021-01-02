93f83db5ecf6a3bd8fca7ec9c08fde551bfdef0e
Ziyue Pan <3180105354@zju.edu.cn>
2021-01-02 17:09:33 +0800

    update Alarmio

---
+++ Alarmio/app/src/main/java/me/jfenn/alarmio/Alarmio.java
@@ -0,0 +1,440 @@
+package me.jfenn.alarmio;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.media.Ringtone;
+import android.net.Uri;
+import android.os.Build;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.multidex.MultiDexApplication;
+
+import com.afollestad.aesthetic.Aesthetic;
+import com.afollestad.aesthetic.AutoSwitchMode;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.audio.AudioAttributes;
+import com.google.android.exoplayer2.source.MediaSourceFactory;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import me.jfenn.alarmio.data.AlarmData;
+import me.jfenn.alarmio.data.PreferenceData;
+import me.jfenn.alarmio.data.SoundData;
+import me.jfenn.alarmio.data.TimerData;
+import me.jfenn.alarmio.services.TimerService;
+import me.jfenn.alarmio.utils.DebugUtils;
+
+public class Alarmio extends MultiDexApplication implements Player.EventListener {
+
+    public static final int THEME_DAY = 0;
+
+    public static final String NOTIFICATION_CHANNEL_TIMERS = "timers";
+
+    private SharedPreferences prefs;
+
+    private Ringtone currentRingtone;
+
+    private List<AlarmData> alarms;
+    private List<TimerData> timers;
+
+    private List<AlarmioListener> listeners;
+    private ActivityListener listener;
+
+    private SimpleExoPlayer player;
+    private HlsMediaSource.Factory hlsMediaSourceFactory;
+    private ProgressiveMediaSource.Factory progressiveMediaSourceFactory;
+    private String currentStream;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DebugUtils.setup(this);
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        listeners = new ArrayList<>();
+        alarms = new ArrayList<>();
+        timers = new ArrayList<>();
+
+        player = new SimpleExoPlayer.Builder(this).build();
+        player.addListener(this);
+
+        DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "exoplayer2example"), null);
+        hlsMediaSourceFactory = new HlsMediaSource.Factory(dataSourceFactory);
+        progressiveMediaSourceFactory = new ProgressiveMediaSource.Factory(dataSourceFactory);
+
+        int alarmLength = PreferenceData.ALARM_LENGTH.getValue(this);
+        for (int id = 0; id < alarmLength; id++) {
+            alarms.add(new AlarmData(id, this));
+        }
+
+        int timerLength = PreferenceData.TIMER_LENGTH.getValue(this);
+        for (int id = 0; id < timerLength; id++) {
+            TimerData timer = new TimerData(id, this);
+            if (timer.isSet())
+                timers.add(timer);
+        }
+
+        if (timerLength > 0)
+            startService(new Intent(this, TimerService.class));
+    }
+
+    public List<AlarmData> getAlarms() {
+        return alarms;
+    }
+
+    public List<TimerData> getTimers() {
+        return timers;
+    }
+
+    /**
+     * Create a new alarm, assigning it an unused preference id.
+     *
+     * @return The newly instantiated [AlarmData](./data/AlarmData).
+     */
+    public AlarmData newAlarm() {
+        AlarmData alarm = new AlarmData(alarms.size(), Calendar.getInstance());
+        alarm.sound = SoundData.fromString(PreferenceData.DEFAULT_ALARM_RINGTONE.getValue(this, ""));
+        alarms.add(alarm);
+        onAlarmCountChanged();
+        return alarm;
+    }
+
+    /**
+     * Remove an alarm and all of its its preferences.
+     *
+     * @param alarm The alarm to be removed.
+     */
+    public void removeAlarm(AlarmData alarm) {
+        alarm.onRemoved(this);
+
+        int index = alarms.indexOf(alarm);
+        alarms.remove(index);
+        for (int i = index; i < alarms.size(); i++) {
+            alarms.get(i).onIdChanged(i, this);
+        }
+
+        onAlarmCountChanged();
+        onAlarmsChanged();
+    }
+
+    /**
+     * Update preferences to show that the alarm count has been changed.
+     */
+    public void onAlarmCountChanged() {
+        PreferenceData.ALARM_LENGTH.setValue(this, alarms.size());
+    }
+
+    /**
+     * Notify the application of changes to the current alarms.
+     */
+    public void onAlarmsChanged() {
+        for (AlarmioListener listener : listeners) {
+            listener.onAlarmsChanged();
+        }
+    }
+
+    /**
+     * Create a new timer, assigning it an unused preference id.
+     *
+     * @return The newly instantiated [TimerData](./data/TimerData).
+     */
+    public TimerData newTimer() {
+        TimerData timer = new TimerData(timers.size());
+        timers.add(timer);
+        onTimerCountChanged();
+        return timer;
+    }
+
+    /**
+     * Remove a timer and all of its preferences.
+     *
+     * @param timer The timer to be removed.
+     */
+    public void removeTimer(TimerData timer) {
+        timer.onRemoved(this);
+
+        int index = timers.indexOf(timer);
+        timers.remove(index);
+        for (int i = index; i < timers.size(); i++) {
+            timers.get(i).onIdChanged(i, this);
+        }
+
+        onTimerCountChanged();
+        onTimersChanged();
+    }
+
+    /**
+     * Update the preferences to show that the timer count has been changed.
+     */
+    public void onTimerCountChanged() {
+        PreferenceData.TIMER_LENGTH.setValue(this, timers.size());
+    }
+
+    /**
+     * Notify the application of changes to the current timers.
+     */
+    public void onTimersChanged() {
+        for (AlarmioListener listener : listeners) {
+            listener.onTimersChanged();
+        }
+    }
+
+    /**
+     * Starts the timer service after a timer has been set.
+     */
+    public void onTimerStarted() {
+        startService(new Intent(this, TimerService.class));
+    }
+
+    /**
+     * Update the application theme.
+     */
+    public void updateTheme() {
+        Aesthetic.Companion.get()
+                .isDark(false)
+                .lightStatusBarMode(AutoSwitchMode.ON)
+                .colorPrimary(ContextCompat.getColor(this, R.color.colorPrimary))
+                .colorStatusBar(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT ? Color.TRANSPARENT : ContextCompat.getColor(this, R.color.colorPrimaryDark))
+                .colorNavigationBar(ContextCompat.getColor(this, R.color.colorPrimaryDark))
+                .colorAccent(ContextCompat.getColor(this, R.color.colorAccent))
+                .colorCardViewBackground(ContextCompat.getColor(this, R.color.colorForeground))
+                .colorWindowBackground(ContextCompat.getColor(this, R.color.colorPrimaryDark))
+                .textColorPrimary(ContextCompat.getColor(this, R.color.textColorPrimary))
+                .textColorSecondary(ContextCompat.getColor(this, R.color.textColorSecondary))
+                .textColorPrimaryInverse(ContextCompat.getColor(this, R.color.textColorPrimaryNight))
+                .textColorSecondaryInverse(ContextCompat.getColor(this, R.color.textColorSecondaryNight))
+                .apply();
+    }
+
+    /**
+     * Determine if a ringtone is currently playing.
+     *
+     * @return True if a ringtone is currently playing.
+     */
+    public boolean isRingtonePlaying() {
+        return currentRingtone != null && currentRingtone.isPlaying();
+    }
+
+    /**
+     * Get the currently playing ringtone.
+     *
+     * @return The currently playing ringtone, or null.
+     */
+    @Nullable
+    public Ringtone getCurrentRingtone() {
+        return currentRingtone;
+    }
+
+    public void playRingtone(Ringtone ringtone) {
+        if (!ringtone.isPlaying()) {
+            stopCurrentSound();
+            ringtone.play();
+        }
+
+        currentRingtone = ringtone;
+    }
+
+    /**
+     * Play a stream ringtone.
+     *
+     * @param url The URL of the stream to be passed to ExoPlayer.
+     * @see [ExoPlayer Repo](https://github.com/google/ExoPlayer)
+     */
+    private void playStream(String url, String type, MediaSourceFactory factory) {
+        stopCurrentSound();
+
+        // Error handling, including when this is a progressive stream
+        // rather than a HLS stream, is in onPlayerError
+        player.prepare(factory.createMediaSource(Uri.parse(url)));
+        player.setPlayWhenReady(true);
+
+        currentStream = url;
+    }
+
+    /**
+     * Play a stream ringtone.
+     *
+     * @param url The URL of the stream to be passed to ExoPlayer.
+     * @see [ExoPlayer Repo](https://github.com/google/ExoPlayer)
+     */
+    public void playStream(String url, String type) {
+        playStream(url, type, hlsMediaSourceFactory);
+    }
+
+    /**
+     * Play a stream ringtone.
+     *
+     * @param url        The URL of the stream to be passed to ExoPlayer.
+     * @param attributes The attributes to play the stream with.
+     * @see [ExoPlayer Repo](https://github.com/google/ExoPlayer)
+     */
+    public void playStream(String url, String type, AudioAttributes attributes) {
+        player.stop();
+        player.setAudioAttributes(attributes);
+        playStream(url, type);
+    }
+
+    /**
+     * Stop the currently playing stream.
+     */
+    public void stopStream() {
+        player.stop();
+        currentStream = null;
+    }
+
+    /**
+     * Sets the player volume to the given float.
+     *
+     * @param volume The volume between 0 and 1
+     */
+    public void setStreamVolume(float volume) {
+        player.setVolume(volume);
+    }
+
+    /**
+     * Determine if the passed url matches the stream that is currently playing.
+     *
+     * @param url The URL to match the current stream to.
+     * @return True if the URL matches that of the currently playing
+     * stream.
+     */
+    public boolean isPlayingStream(String url) {
+        return currentStream != null && currentStream.equals(url);
+    }
+
+    /**
+     * Stop the currently playing sound, regardless of whether it is a ringtone
+     * or a stream.
+     */
+    public void stopCurrentSound() {
+        if (isRingtonePlaying())
+            currentRingtone.stop();
+
+        stopStream();
+    }
+
+    public void addListener(AlarmioListener listener) {
+        listeners.add(listener);
+    }
+
+    public void removeListener(AlarmioListener listener) {
+        listeners.remove(listener);
+    }
+
+    public void setListener(ActivityListener listener) {
+        this.listener = listener;
+
+        if (listener != null)
+            updateTheme();
+    }
+
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        switch (playbackState) {
+            case Player.STATE_BUFFERING:
+            case Player.STATE_READY:
+                // We are idle while switching from HLS to Progressive streaming
+            case Player.STATE_IDLE:
+                break;
+            default:
+                currentStream = null;
+                break;
+        }
+    }
+
+    @Override
+    public void onRepeatModeChanged(int repeatMode) {
+    }
+
+    @Override
+    public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+        String lastStream = currentStream;
+        currentStream = null;
+        Exception exception;
+        switch (error.type) {
+            case ExoPlaybackException.TYPE_RENDERER:
+                exception = error.getRendererException();
+                break;
+            case ExoPlaybackException.TYPE_SOURCE:
+                if (lastStream != null && error.getSourceException().getMessage().contains("does not start with the #EXTM3U header")) {
+                    playStream(lastStream, SoundData.TYPE_RADIO, progressiveMediaSourceFactory);
+                    return;
+                }
+                exception = error.getSourceException();
+                break;
+            case ExoPlaybackException.TYPE_UNEXPECTED:
+                exception = error.getUnexpectedException();
+                break;
+            default:
+                return;
+        }
+
+        exception.printStackTrace();
+        Toast.makeText(this, exception.getClass().getName() + ": " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPositionDiscontinuity(int reason) {
+    }
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+    }
+
+    @Override
+    public void onSeekProcessed() {
+    }
+
+    public void requestPermissions(String... permissions) {
+        if (listener != null)
+            listener.requestPermissions(permissions);
+    }
+
+    public FragmentManager getFragmentManager() {
+        if (listener != null)
+            return listener.gettFragmentManager();
+        else return null;
+    }
+
+    public interface AlarmioListener {
+        void onAlarmsChanged();
+
+        void onTimersChanged();
+    }
+
+    public interface ActivityListener {
+        void requestPermissions(String... permissions);
+
+        FragmentManager gettFragmentManager(); //help
+    }
+
+}
---
+++ Alarmio/app/src/main/java/me/jfenn/alarmio/fragments/HomeFragment.java
@@ -0,0 +1,205 @@
+package me.jfenn.alarmio.fragments;
+
+import android.Manifest;
+import android.app.AlarmManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
+
+import com.afollestad.aesthetic.Aesthetic;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.Calendar;
+
+import io.reactivex.disposables.Disposable;
+import jahirfiquitiva.libs.fabsmenu.FABsMenu;
+import jahirfiquitiva.libs.fabsmenu.FABsMenuListener;
+import jahirfiquitiva.libs.fabsmenu.TitleFAB;
+import me.jfenn.alarmio.Alarmio;
+import me.jfenn.alarmio.R;
+import me.jfenn.alarmio.adapters.SimplePagerAdapter;
+import me.jfenn.alarmio.data.AlarmData;
+import me.jfenn.alarmio.dialogs.AestheticTimeSheetPickerDialog;
+import me.jfenn.alarmio.dialogs.TimerDialog;
+import me.jfenn.alarmio.fragments.Dashboard.DashboardFragment;
+import me.jfenn.timedatepickers.dialogs.PickerDialog;
+import me.jfenn.timedatepickers.views.LinearTimePickerView;
+
+public class HomeFragment extends BaseFragment {
+
+    public static final String INTENT_ACTION = "me.jfenn.alarmio.HomeFragment.INTENT_ACTION";
+
+    private View view;
+    private FABsMenu menu;
+    private TitleFAB timerFab;
+    private TitleFAB alarmFab;
+
+
+    private Disposable colorAccentSubscription;
+    private Disposable textColorPrimarySubscription;
+    private Disposable textColorPrimaryInverseSubscription;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        ViewPager viewPager = view.findViewById(R.id.viewPager);
+        TabLayout tabLayout = view.findViewById(R.id.tabLayout);
+        menu = view.findViewById(R.id.fabsMenu);
+        timerFab = view.findViewById(R.id.timerFab);
+        alarmFab = view.findViewById(R.id.alarmFab);
+
+
+        SimplePagerAdapter pagerAdapter = new SimplePagerAdapter(
+                getContext(), getChildFragmentManager(),
+                new AlarmsFragment.Instantiator(getContext()),
+                new DashboardFragment.Instantiator(getContext()),
+                new SettingsFragment.Instantiator(getContext())
+        );
+
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() > 0) {
+                    menu.hide();
+                } else {
+                    menu.show();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+
+
+        view.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                view.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+
+
+        colorAccentSubscription = Aesthetic.Companion.get()
+                .colorAccent()
+                .subscribe(integer -> {
+                    menu.setMenuButtonColor(integer);
+
+                    int color = ContextCompat.getColor(getContext(), R.color.textColorPrimary);
+                    menu.getMenuButton().setColorFilter(color);
+                    timerFab.setColorFilter(color);
+                    alarmFab.setColorFilter(color);
+
+                    timerFab.setBackgroundColor(integer);
+                    alarmFab.setBackgroundColor(integer);
+                });
+
+        textColorPrimarySubscription = Aesthetic.Companion.get()
+                .textColorPrimary()
+                .subscribe(integer -> {
+                    timerFab.setTitleTextColor(integer);
+                    alarmFab.setTitleTextColor(integer);
+                });
+
+        textColorPrimaryInverseSubscription = Aesthetic.Companion.get()
+                .textColorPrimaryInverse()
+                .subscribe(integer -> {
+                    alarmFab.setTitleBackgroundColor(integer);
+                    timerFab.setTitleBackgroundColor(integer);
+                });
+
+        timerFab.setOnClickListener(view -> {
+            invokeTimerScheduler();
+            menu.collapse();
+        });
+
+        alarmFab.setOnClickListener(view -> {
+            invokeAlarmScheduler();
+            menu.collapse();
+        });
+
+        menu.setMenuListener(new FABsMenuListener() {
+            @Override
+            public void onMenuExpanded(FABsMenu fabsMenu) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.FOREGROUND_SERVICE) != PackageManager.PERMISSION_GRANTED)
+                        requestPermissions(new String[]{Manifest.permission.FOREGROUND_SERVICE}, 0);
+                    else fabsMenu.collapseImmediately();
+                }
+            }
+        });
+
+        // check actions passed from MainActivity; open timer/alarm schedulers if necessary
+        Bundle args = getArguments();
+        String action = args != null ? args.getString(INTENT_ACTION, null) : null;
+        if (AlarmClock.ACTION_SET_ALARM.equals(action)) {
+            view.post(() -> invokeAlarmScheduler());
+        } else if (AlarmClock.ACTION_SET_TIMER.equals(action)) {
+            view.post(() -> invokeTimerScheduler());
+        }
+
+        return view;
+    }
+
+    /**
+     * Open the alarm scheduler dialog to allow the user to create
+     * a new alarm.
+     */
+    private void invokeAlarmScheduler() {
+        new AestheticTimeSheetPickerDialog(view.getContext())
+                .setListener(new PickerDialog.OnSelectedListener<LinearTimePickerView>() {
+                    @Override
+                    public void onSelect(PickerDialog<LinearTimePickerView> dialog, LinearTimePickerView view) {
+                        AlarmManager manager = (AlarmManager) view.getContext().getSystemService(Context.ALARM_SERVICE);
+                        AlarmData alarm = getAlarmio().newAlarm();
+                        alarm.time.set(Calendar.HOUR_OF_DAY, view.getHourOfDay());
+                        alarm.time.set(Calendar.MINUTE, view.getMinute());
+                        alarm.setTime(getAlarmio(), manager, alarm.time.getTimeInMillis());
+                        alarm.setEnabled(getContext(), manager, true);
+
+                        getAlarmio().onAlarmsChanged();
+                    }
+
+                    @Override
+                    public void onCancel(PickerDialog<LinearTimePickerView> dialog) {
+                    }
+                })
+                .show();
+    }
+
+    /**
+     * Open the timer scheduler dialog to allow the user to start
+     * a timer.
+     */
+    private void invokeTimerScheduler() {
+        new TimerDialog(getContext(), getFragmentManager())
+                .show();
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        colorAccentSubscription.dispose();
+        textColorPrimarySubscription.dispose();
+        textColorPrimaryInverseSubscription.dispose();
+        super.onDestroyView();
+    }
+
+}
