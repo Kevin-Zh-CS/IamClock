cbce00a8f73b86a8df4865f68f748964da06ca89
Ziyue Pan <3180105354@zju.edu.cn>
2021-01-06 20:29:02 +0800

    auto report done

--- Alarmio/app/src/main/java/me/jfenn/alarmio/utils/AlarmException.java
+++ Alarmio/app/src/main/java/me/jfenn/alarmio/utils/AlarmException.java
@@ -1,4 +1,14 @@
 package me.jfenn.alarmio.utils;
 
-public class AlarmException {
+public class AlarmException extends Exception {
+    private String msg;
+
+    public AlarmException(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return msg;
+    }
 }
